720d955e7fa8bbe8ed4b24e018a63e61795979b9
Marzz616 <mariobusbys@gmail.com>
2024-06-17 13:24:03 +0200

    finished informatie pagina

--- Main/informatie.php
+++ Main/informatie.php
@@ -3,9 +3,13 @@
   <link rel="stylesheet" href="assets/css/informatie.css">
 
   <?php include("assets/includes/header.php"); ?>
+
+  
   <div class="hero-image">
         <div class="hero-text">
-            
+            <h1>Welcome to Day Trading</h1>
+            <p>Learn the market trends and become a pro</p>
+            <button onclick="window.location.href='contact.html'">schrijf je Nu in</button>
         </div>
     </div>
     <main>
@@ -17,83 +21,113 @@
                 <p class="ColorG">Day Trading en Market Trends</p>
                 <br>
                 <div class="insider">
-                <div class="info-1 ">
-                    <p class="">
+                    <div class="info-1">
                         <ul>
-
-                      <li> NIVEAU 4 </li>
-
-                      <li> BOL </li>
-                      
-                      <li> 3 JR </li>
-                    </ul>
-                </p>
-              
-                  </div>
+                            <li>NIVEAU 4</li>
+                            <li>BOL</li>
+                            <li>3 JR</li>
+                        </ul>
+                    </div>
                 </div>
                 <br>
-        
-            <div class="insider2">
-                <li><label>academie</label><strong>ICT Academia</strong></li>
-            </div>
-            <div class="insider2">
-                <li><label>locatie</label><strong>Australiëlaan 25, Utrecht</strong></li>
-            </div>    
-            <div class="insider2">
-                <li><label> crebo nr./naam</label><strong>26000 / Day Trading & Market Trends</strong></li>
-            </div>
-            <div class="insider2">
-                <li><label>MHBO</label><strong>Nee</strong></li>
-            </div>
-                
-            <div class="insider2">
-                <li><label>versnellen</label><strong>Ja, in overleg met de opleiding</strong></li>
-            </div>    
                 <div class="insider2">
-                <li> <label>kosten</label>Voor deze opleiding betaal je lesgeld als je op 1 augustus ouder bent dan 18 jaar. Verder moet je rekening houden met de aanschaf van een laptop en kosten voor boeken en andere leermiddelen. Daarvoor moet je rekening houden met onderstaande kosten (ongeveer):
-                </li>
+                    <li><label>academie</label><strong>ICT Academia</strong></li>
+                </div>
+                <div class="insider2">
+                    <li><label>locatie</label><strong>Australiëlaan 25, Utrecht</strong></li>
+                </div>    
+                <div class="insider2">
+                    <li><label>crebo nr./naam</label><strong>26000 / Day Trading & Market Trends</strong></li>
+                </div>
+                <div class="insider2">
+                    <li><label>MHBO</label><strong>Nee</strong></li>
+                </div>
+                <div class="insider2">
+                    <li><label>versnellen</label><strong>Ja, in overleg met de opleiding</strong></li>
+                </div>    
+                <div class="insider2">
+                    <li>
+                        <label>kosten</label>Voor deze opleiding betaal je lesgeld als je op 1 augustus ouder bent dan 18 jaar. Verder moet je rekening houden met de aanschaf van een laptop en kosten voor boeken en andere leermiddelen. Daarvoor moet je rekening houden met onderstaande kosten (ongeveer):
+                    </li>
                 </div>
                 <br>
                 <div>
-                   
                     <ul>
-                    <li>Leerjaar 1: €270,-</li>
-                    <li>Leerjaar 2: €70,-</li>
-                    <li>Leerjaar 3: €70,-</li>
-                    <li>Laptop: €900,-</li>
+                        <li>Leerjaar 1: €270,-</li>
+                        <li>Leerjaar 2: €70,-</li>
+                        <li>Leerjaar 3: €70,-</li>
+                        <li>Laptop: €900,-</li>
                     </ul>
                 </div>
-            
                 <br>
                 <br>
-
                 <div class="video">
-
-                    <iframe width="625px" height="351px"  src="https://www.youtube.com/embed/I9unIJnobQU" frameborder="0.1"></iframe>
-                   
-                  </div>
-
+                    <iframe width="625px" height="400px" src="https://www.youtube.com/embed/I9unIJnobQU" frameborder="0.1"></iframe>
+                </div>
                 <div class="button-container">
                     <button onclick="window.location.href='contact.html'">schrijf je Nu in</button>
                 </div>
-                </div>
-            </section>
+            </div>
+        </section>
+        <section class="containerB">
+            <div class="box">
+                <h5 class="insider3">opleiding</h5>
+            <br>
+                <div>
+            <p><strong>
+                Deze cursus biedt studenten hands-on ervaring, praktische inzichten en deskundige begeleiding om de vaardigheden te ontwikkelen die nodig zijn om succesvolle daghandelaren te worden. 
+                Afgestudeerden zullen goed voorbereid zijn om een carrière in daghandel na te streven of hun eigen handelsbedrijf te starten. 
+                Let op dat daghandelen hoge risico's met zich meebrengt en niet geschikt is voor iedereen.
+                 Het vereist 
+                 discipline, focus en continu leren om te slagen in dit competitieve vakgebied.
+            </strong></p>
+                <br>
 
-            
-            <section class="containerB">
+                <h6>Jaar 1: Fundamenten van Daghandelen</h6>
+            <p>
+                Kennismaking met Financiële Markten
+                Basisprincipes van Technische Analyse
+                Begrip van Marktindicators
+                Risicomanagementstrategieën
+                Kennismaking met Handelsplatforms
+                Oefenen met Handelssimulaties
+            </p>
 
-                <div class="box">
+                <br>
 
-                    <h5 class="insider3">opleiding</h5>
-                </div>
-
-            <p></p>
-
-            </section>
-    
+                <h6>Jaar 2: Geavanceerde Daghandelstechnieken</h6>
+            <p>
+                Geavanceerde Technische Analyse
+                Grafiekpatronen en Trendanalyse
+                Scalping- en Momentumhandelsstrategieën
+                Opties en Futures Handel
+                Hefboomwerking en Marginhandel
+                Ontwikkelen van een Persoonlijk Handelsplan
+            </p>
+                <br>
+                <h6>Jaar 3: Professionele Daghandelsstrategieën</h6>
+                
+            <p> Algoritmisch Handelen en Geautomatiseerde Systemen
+                Geavanceerde Risicobeheertechnieken
+                Handelspsychologie en Emotionele Discipline
+                Opbouwen van een Handelsportefeuille
+                Terugtesten en Optimalisatie
+                Praktijkervaring en Stage in de Echte Wereld
+            </p>
+                
+                <br>
+                <h6>Benodigdheden</h6>
+            <p>
+                Laptop met internettoegang
+                Handelssoftware (verstrekt door de school)
+                Notitieboekje en pen voor aantekeningen
+                Handelsaccount bij een effectenmakelaarsbedrijf (oefenaccount verstrekt)
+                Aanbevolen leesmateriaal en studieboeken (verstrekt door de school)
+            </p>
+            </div>
+            </div>
+        </section>
     </main>
-    <br>
-    <br>
  
 
   <?php include("assets/includes/footer.php") ?>
